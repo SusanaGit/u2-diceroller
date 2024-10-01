6d9c91108861bd3a21e65404c87c9301e7d177bf
SusanaGit <169191115+SusanaGit@users.noreply.github.com>
2024-10-01 10:12:37 +0200

    align center

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -26,6 +32,7 @@ class MainActivity : ComponentActivity() {
 private fun DiceWithButtonAndImage(
     modifier: Modifier = Modifier
         .fillMaxSize()
+        .wrapContentSize(Alignment.Center)
 ) {
 
 }
